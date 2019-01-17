68d033fcd822c15b9801a30989bd1ca7d829840b
folko11 <foltan.milosml@gmail.com>
2019-01-17 12:25:26 +0100

    oprava nacitania fotiek

--- data.py
+++ data.py
@@ -17,11 +17,11 @@ class Data:
         try:
             self.train_data = []
             self.train_labels = []
-            for i in range(0,3):
+            for i in range(0,4):
                 img = Image.open('data/ISIC_000000' + str(i) +'.jpg')
 
                 self.train_data.append(np.array(img.resize((conf.IMG_SIZE_X,conf.IMG_SIZE_Y),Image.ANTIALIAS)))
-                img = np.expand_dims(img, axis=0)
+                #img = np.expand_dims(img, axis=0)
                 arr = self.train_data[0]
                 if i > 1 :
                     self.train_labels.append(np.array([0]))
@@ -29,12 +29,6 @@ class Data:
                     self.train_labels.append(np.array([ 1]))
 
                 print(arr.shape)
-
-            #self.show_image(self.train_data[0])
-            #for i in range(0,3):
-             #   self.train_data[i] = self.train_data[i].reshape(-1, conf.IMG_SIZE_X, conf.IMG_SIZE_Y, 1)
-              #  print(self.train_data[i].shape)
-
 
         except IOError:
             print("Chyba pri otvarani suboru! Skontrolujte cestu v configuration.py"+
