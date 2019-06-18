1340ee95f4afb1a89da8c328a460e2409b8ee32c
folko11 <foltan.milosml@gmail.com>
2019-06-18 20:42:17 +0200

    refactor

---
+++ config.py
@@ -0,0 +1,22 @@
+import math
+
+## global params
+theta = - math.pi / 8
+ratio = 0.7
+bounding_box_margin = 10
+inter_layer_margin = 50
+text_margin = 50
+channel_scale = 0.5
+text_size = 10
+one_dim_width = 20
+line_color_feature_map = (0, 0, 0)
+line_color_layer = (0, 0, 255)
+text_color_feature_map = (0, 0, 0)
+text_color_layer = (0, 0, 0)
+
+## netwok params
+IMG_SIZE_X = 450 #450
+IMG_SIZE_Y = 600 #300
+EPOCH = 25
+TRAIN_DATA = 80
+TEST_DATA = 20
