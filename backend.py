efd6350a685dff598e8b4f577a162f15e0be310a
sarina3 <sarinajakub@gmail.com>
2019-06-25 21:53:33 +0200

    image send complete

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+from flask import Flask,request
 import flask
 #import application as neuralNetworkApplication
 import nn_type
@@ -7,8 +8,8 @@ from flask_cors import CORS
 import csv
 
 # server application instance
-app = flask.Flask(__name__)
-CORS(app)
+app = Flask(__name__)
+CORS(app, resources={"*": {"origins": "*"}})
 # holds instance of application with neural network
 #application = neuralNetworkApplication.Application(True)
 
@@ -24,25 +25,40 @@ CORS(app)
 ## VZOROVY POPIS SERVICE
 # - input params
 # - output data vo forme json
-@app.route("/loadImage",methods=["POST", "OPTIONS"])
+@app.route("/loadImages",methods=["POST"])
 def loadImages():
+    req = request.get_json()
+    
     result = []
-    with open('images_other/description(MSK-1)/metadata.csv','r') as metadata:
-        reader = csv.reader(metadata)
+    with open('dataset/cnn/description/metadata.csv','r') as metadata:
         data = []
-        for row in reader:
-            data.append({'name': row[0], 'diagnosis': row[0]})
+        for line in metadata:
+            tmp = line.split(';')
+            if tmp[0] == 'name':
+                continue
+            data.append({'name': tmp[0]+'.jpg', 'diagnosis': tmp[0]})
+        low = req.get('lastIndex')
+        high = low + req.get('count') 
+        for i in range(low, high):
+            if i > len(data):
+                break
+            with open('dataset/cnn/images/'+data[i]['name'], 'rb') as file:
+                tmp = base64.b64encode(file.read())
+                result.append(tmp.decode('utf-8'))
+        metadata = {}
+        metadata['lastIndex'] = high
+        metadata['count'] = len(result)
+        metadata['all'] = len(data)
+        jsonData = json.dumps({'data': result,'metadata': metadata})
+        return flask.make_response(jsonData)
+#    with open('dataset/cnn/images/ISIC_0024306.jpg', 'rb') as file:
+#         tmp = base64.b64encode(file.read())
+#         result.append(tmp.decode('utf-8'))
 
-        req = flask.request.data['metadata']
-    with open('dataset/cnn/images/ISIC_0024306.jpg', 'rb') as file:
-        tmp = base64.b64encode(file.read())
-        result.append(tmp.decode('utf-8'))
-
-    with open('dataset/cnn/images/ISIC_0024307.jpg', 'rb') as file:
-        tmp = base64.b64encode(file.read())
-        result.append(tmp.decode('utf-8'))
-        jsonData = json.dumps({'data': result})
-    return flask.make_response(jsonData)
+#     with open('dataset/cnn/images/ISIC_0024307.jpg', 'rb') as file:
+#         tmp = base64.b64encode(file.read())
+#         result.append(tmp.decode('utf-8'))
+#         
 
 
 @app.route("/predict",methods=["POST"])
