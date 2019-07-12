1143d0cfbe9d3816649648ae17742f41508b9ecf
folko11 <foltan.milosml@gmail.com>
2019-07-12 21:52:00 +0200

    samostatne spustenie

--- maintenance.py
+++ maintenance.py
@@ -1,19 +1,36 @@
 import os
 
+path = 'dataset/cnn/'
 
-path = 'dataset/tmp/'
+# rozdelenie celeho setu na zlozky podla diagnoz
+def split_image_by_diagnoses():
+    with open(path+'/description(10015)/metadata_with_X.csv', 'r') as metadata:
+        #os.mkdir(path+'processed')
+        #os.mkdir(path+'processed/malignant')
+        #os.mkdir(path+'processed/benign')
+        #os.mkdir(path+'processed/unclassed')     ## pre tie co nemaju diagnozu
+        map = ['benign/','malignant/']
+        for line in metadata:
+            data = line.split(';')
+            with open(path+'/images/'+data[0], 'rb') as file:
+                filedata = file.read()
+                with open(path+'/processed/'+map[int(data[1])]+ data[0], 'wb') as newfile:
+                    newfile.write(filedata)
+                    newfile.close()
+                file.close()
+            os.remove(path+'images/'+data[0])
 
-with open(path+'/description/metadata.csv', 'r') as metadata:
-    os.mkdir(path+'processed')
-    os.mkdir(path+'processed/malignant')
-    os.mkdir(path+'processed/benign')
-    map = ['benign/','malignant/']
-    for line in metadata:
-        data = line.split(';')
-        with open(path+'/images/'+data[0], 'rb') as file:
-            filedata = file.read()
-            with open(path+'/processed/'+map[int(data[1])]+ data[0], 'wb') as newfile:
-                newfile.write(filedata)
-                newfile.close()
-            file.close()
-        os.remove(path+'images/'+data[0])
+# rozdelenie processed obrazkov na podtriedy
+def split_images_into_sets():
+    os.mkdir(path + 'train')
+    os.mkdir(path + 'train/malignant')
+    os.mkdir(path + 'train/benign')
+    os.mkdir(path + 'test')
+    os.mkdir(path + 'test/malignant')
+    os.mkdir(path + 'test/benign')
+    os.mkdir(path + 'validation')
+    os.mkdir(path + 'validation/malignant')
+    os.mkdir(path + 'validation/benign')
+
+if __name__ == '__main__':
+    split_image_by_diagnoses()
