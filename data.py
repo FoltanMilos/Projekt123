7d8aafcdf5badbaeba0f66ee6bc5f637f324c858
folko11 <foltan.milosml@gmail.com>
2019-07-12 13:01:08 +0200

    Nacitanie dat ako flow, zmena celej triedy

--- data.py
+++ data.py
@@ -4,27 +4,28 @@ import os
 import numpy as np
 import glob
 import config as conf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image as img_proc
 
-
-##ALL data informations and datasets with its labels
 class Data:
-    global train_data
-    global train_labels
+    global train_set
+    #global train_labels
+    global valid_set
+    #global valid_labels
+    global test_set
+    # global test_labels
 
-    global test_data
-    global test_labels
-
-    global imagTrain
-    global imagTest
+    # file iterator
+    global train_datagen
 
     def __init__(self):
-        self.train_data = []
-        self.train_labels = []
-        self.test_data = []
-        self.test_labels = []
-        self.imagTrain = []
-        self.imagTest = []
-
+        self.train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                                shear_range=0.2,
+                                                zoom_range=0.2,
+                                                horizontal_flip=True)
+        self.load_train_set()
+        self.load_test_set()
+        self.load_validation_set()
     ## NACITANIE DAT A ROZDELENIE DO DATASETOV podla conf
     # ak je treba nahrat aj trenovacie data TRUE len ked sa pusta aj trenovanie
     # ked sa pusta testovanie bude sa to nahravat postupne
@@ -76,3 +77,60 @@ class Data:
         except:
             print("Nepodarilo sa nahrat fotku! ")
         return img
+
+    ## NEW PART - NEW LOADING AS FILE ITERATOR
+    def load_train_set(self):
+        self.train_set = self.train_datagen.flow_from_directory(
+            'C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\train\\',
+            target_size=(64, 64),
+            batch_size=16,
+            shuffle=False,
+            #save_to_dir='C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\ssss',
+            classes=["malignant", "bening"],
+            class_mode='binary')
+        return self.train_set
+
+    def load_test_set(self):
+        self.test_set = self.train_datagen.flow_from_directory(
+            'C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\test\\',
+            target_size=(64, 64),
+            batch_size=16,
+            shuffle=False,
+            classes=["malignant", "bening"],
+            class_mode='binary')
+        return self.test_set
+
+    def load_validation_set(self):
+        self.valid_set = self.train_datagen.flow_from_directory(
+            'C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\validation\\',
+            target_size=(64, 64),
+            batch_size=16,
+            shuffle=False,
+            classes=["malignant", "bening"],
+            class_mode='binary')
+        return self.valid_set
+
+    def load_image_exante_flow(self):
+        images_exante_set = self.train_datagen.flow_from_directory(
+            'C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\test\\',
+            target_size=(64, 64),
+            batch_size=1,
+            classes=["malignant", "bening"],
+            shuffle=False,  # pre zachovanie poradia
+            class_mode='binary')
+        return images_exante_set
+
+    def load_image_exante(self):
+        image_exante = img_proc.load_img(
+            'C:\\SKOLA\\7.Semester\\Projekt 1\\SarinaKristaTi\\Projekt123\\dataset\\cnn\\prediction\\malignant\\ISIC_0028679.jpg',
+            target_size=(64, 64))
+        image_exante = img_proc.img_to_array(image_exante)
+        image_exante = np.expand_dims(image_exante, axis=0)
+        return image_exante
+
+    def preproces_image(self,image):
+        image = img_proc.img_to_array(image)
+        image = image.reshape(shape=(64,64)) #OTESTOVAT
+        image = np.expand_dims(image, axis=0)
+        return image
+
