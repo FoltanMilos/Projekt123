6ce5e86812055d45942b3b97765865f72e2f6e18
folko11 <foltan.milosml@gmail.com>
2019-07-15 11:16:37 +0200

    Pridanie rozdelenia datasetov podla absolutnych cisel

--- config.py
+++ config.py
@@ -22,6 +22,20 @@ TRAIN_DATA = 80
 TEST_DATA = 20
 learning_coef = 0.01 ## nie je treba pri ADADELTE
 
+# datasety  - absolutne cisla
+# spolu 10015   - Ben[6705] malig[1113] neurcene[2197]
+# urcene -7818-
+# na sety: 		Train[]   Test[]    Valid[]
+train_ratio = 6000  #76.75
+count_train_malig = 854
+count_train_benign = 5146
+valid_ratio = 1000   #12.8
+count_valid_malig = 142
+count_valid_benign = 858
+test_ratio = 818
+count_test_malig = 117
+count_test_benign = 701
+
 ##program params
 load_model = False   # TRUE trenuje novy model
 				# False nacita loadnuty
