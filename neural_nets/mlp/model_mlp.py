cbebdc63186842584b40b5d5f48b9c9b966fdc96
Mato <matej.kotora96@gmail.com>
2019-09-25 12:26:35 +0200

    domarhany interface

--- neural_nets/mlp/model_mlp.py
+++ neural_nets/mlp/model_mlp.py
@@ -1,28 +1,35 @@
 import interface.model_interface as interface
+from neural_nets.mlp.cv_wrapper import CrossValidation
+from neural_nets.mlp.mlp import Mlp
+
 
 class Model_mlp(interface.ModelInterface):
+    def load_state(self, state):
+        pass
+
+    def save_state(self):
+        pass
 
     def __init__(self):
-        pass
+        self.mlp = None
 
     def test_model(self, test_data, test_labels):
-        pass
+        return CrossValidation.cross_validate_mlp(1, test_data, test_labels, self.mlp)
 
-    def create_model(self):
-        pass
+    def create_model(self, layer_sizes, learning_rate=0.5, activation_function="sigmoid", epoch_count=10):
+        self.mlp = Mlp(layer_sizes, learning_rate, activation_function, epoch_count)
 
     def predict_image(self, img):
-        pass
+        raise Exception("Unsupported function")
 
     def model_summary(self):
-        pass
+        raise Exception("Unsupported function")
 
     def train(self, train_data, train_labels):
-        pass
+        self.mlp.learn(train_data, train_labels)
 
     def load_model(self):
-        pass
+        self.mlp.load_model()
 
     def save_model(self):
-        pass
-
+        self.mlp.save_model()
